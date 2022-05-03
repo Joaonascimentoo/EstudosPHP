f6bfa390e4afc84a125aa695a232332429bb4697
Joao Pedro Muniz Nascimento <jpedro747.jp@gmail.com>
2022-05-03 12:28:27 -0300

    Abordando conceitos/herança/ encapsulamento/acesso

---
+++ poo/src/Pessoa.php
@@ -0,0 +1,39 @@
+<?php
+
+class Pessoa 
+{
+    protected string $nome;
+    private CPF $cpf;
+
+    public function __construct(string $nome, CPF $cpf)
+    {
+        $this->validaNomeTitular($nome);
+        $this-> nome = $nome;
+        $this-> cpf = $cpf;
+    }
+
+    public function retornaNome(): string
+    {
+        return $this-> nome;
+    }
+
+    public function retornaCpf(): string
+    {
+        return $this->cpf->retornarNumero();
+    }
+
+    protected function validaNomeTitular(string $nomeTitular)
+    {
+        if (strLen($nomeTitular) < 5) {
+            echo "O nome do titular precisar ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+}
+
+
+
+
+
+
+?>
--- poo/src/Titular.php
+++ poo/src/Titular.php
@@ -1,39 +1,20 @@
 <?php 
 
-class Titular{
+class Titular extends Pessoa{
     
-    private string $nome;
-    private $cpf;
     private $endereco;
 
     public function __construct(string $nome,CPF $cpf, Endereco $endereco)
     {
-        $this-> cpf = $cpf;
-        $this-> validaNomeTitular($nome);
-        $this-> nome = $nome;
+        parent::__construct($nome,$cpf);
         $this-> endereco = $endereco;
 
 
     }
-    public function recuperaCpf() : string
-    {
-        return $this->cpf->retornarNumero();
-    }
-    public function recuperaNome() : string
-    {
-        return $this->nome;
-    }
+
     public function recuperaEndereco(): Endereco
     {
         return $this->endereco;
-    }
-
-    private function validaNomeTitular($nomeTitular)
-    {
-        if (strLen($nomeTitular) < 5) {
-            echo "O nome do titular precisar ter pelo menos 5 caracteres";
-            exit();
-        }
     }
 
 
