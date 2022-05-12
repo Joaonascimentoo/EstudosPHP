d707facc40edd4f97f2b467af573d55ed20fdb6c
Joao Pedro Muniz Nascimento <jpedro747.jp@gmail.com>
2022-05-11 23:24:08 -0300

    transformando em classe

--- composer/buscador-cursos.php
+++ composer/buscador-cursos.php
@@ -1,23 +1,21 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'src/Buscador.php';
 
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['verify' => false]);
-$resposta = $client->request('GET','https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar("https://www.alura.com.br/cursos-online-programacao/php");
 
 foreach ($cursos as $curso)
 {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso . PHP_EOL;
 }
 
 
